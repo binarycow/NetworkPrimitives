d84de97ace727fb1644d7f28486e09ab60039c7a
Mike Christiansen <michael.a.christiansen10@gmail.com>
2021-11-18 20:02:26 -0500

    Add additional IPv6 types

---
+++ src/NetworkPrimitives.Ipv6/Formatting/Ipv6Parsing.cs
@@ -0,0 +1,101 @@
+﻿#nullable enable
+
+using System;
+using System.Buffers.Binary;
+using System.Runtime.InteropServices;
+using NetworkPrimitives.Utilities;
+
+namespace NetworkPrimitives.Ipv6
+{
+    internal static class Ipv6Parsing
+    {
+        internal static bool TryParseIpv6Address(SpanWrapper text, out int charsRead, out ulong high, out ulong low)
+        {
+            high = default;
+            low = default;
+            charsRead = default;
+            var doubleColonPos = text.IndexOf("::");
+            if (doubleColonPos == -1)
+                return TryParseUnabbreviated(text, out charsRead, out high, out low);
+            var highSpan = text[..doubleColonPos];
+            var lowSpan = text[(doubleColonPos + 2)..];
+            var highGroupCount = GetColonCount(highSpan) + 1;
+            var lowGroupCount = GetColonCount(lowSpan) + 1;
+            Span<ushort> bitGroups = stackalloc ushort[8];
+            if (highGroupCount + lowGroupCount > 8)
+            {
+                return false;
+            }
+            var highGroups = bitGroups[..highGroupCount];
+            var lowGroups = bitGroups[^lowGroupCount..];
+            if (!TryFillBitGroups(highSpan, ref charsRead, highGroups))
+            {
+                return false;
+            }
+            if (!TryFillBitGroups(lowSpan, ref charsRead, lowGroups))
+            {
+                return false;
+            }
+            return Create(bitGroups, out high, out low);
+
+            static bool TryFillBitGroups(SpanWrapper text, ref int charsRead, Span<ushort> bitGroups)
+            {
+                while (bitGroups.Length > 0 && text.TryParseHexUshort(ref charsRead, out var bitGroup))
+                {
+                    bitGroups[0] = bitGroup;
+                    bitGroups = bitGroups[1..];
+                    if (text.Length == 0 || text[0] != ':')
+                        break;
+                    text = text[1..];
+                    ++charsRead;
+                }
+                return bitGroups.Length == 0;
+            }
+            static int GetColonCount(SpanWrapper text)
+            {
+                var ct = 0;
+                for (var i = 0; i < text.Length; ++i)
+                {
+                    if (text[i] == ':')
+                        ++ct;
+                }
+                return ct;
+            }
+        }
+        
+        
+        internal static bool TryParseUnabbreviated(SpanWrapper text, out int charsRead, out ulong high, out ulong low)
+        {
+            charsRead = default;
+            Span<ushort> bitGroups = stackalloc ushort[8];
+            // Read the first bit groups
+            var startLength = 0;
+            while (text.TryParseHexUshort(ref charsRead, out var bitGroup))
+            {
+                bitGroups[startLength++] = bitGroup;
+                if (text.Length == 0 || text[0] != ':')
+                    break;
+                text = text[1..];
+                ++charsRead;
+            }
+            if (startLength == 8) 
+                return Create(bitGroups, out high, out low);
+            high = default;
+            low = default;
+            return false;
+        }
+        
+        
+
+        private static bool Create(Span<ushort> bitGroups, out ulong high, out ulong low)
+        {
+            high = Convert(bitGroups[..4]);
+            low = Convert(bitGroups[4..]);
+            return true;
+            static ulong Convert(Span<ushort> ushorts) 
+                => BinaryPrimitives.ReadUInt64BigEndian(
+                    MemoryMarshal.Cast<ushort, Byte>(ushorts)
+                );
+        }
+    }
+}
---
+++ src/NetworkPrimitives.Ipv6/Subnet/Ipv6Cidr.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using NetworkPrimitives.Utilities;
+
+namespace NetworkPrimitives.Ipv6
+{
+    public readonly struct Ipv6Cidr : ITryFormat, IEquatable<Ipv6Cidr>
+    {
+        internal byte Value { get; }
+        private Ipv6Cidr(byte value) => this.Value = value;
+        int ITryFormat.MaximumLengthRequired => 3;
+
+        public static implicit operator Ipv6Cidr(Ipv6SubnetMask mask)
+        {
+            var (low, high) = mask;
+            var cidr = SubnetMaskLookups.GetCidr(high, low);
+            return new Ipv6Cidr(cidr);
+        }
+        public bool TryFormat(Span<char> destination, out int charsWritten) 
+            => Value.TryFormat(destination, out charsWritten);
+
+        public override string ToString() => this.GetString();
+        public static explicit operator byte(Ipv6Cidr cidr) => cidr.Value;
+
+        public bool Equals(Ipv6Cidr other) => this.Value == other.Value;
+        public override bool Equals(object? obj) => obj is Ipv6Cidr other && Equals(other);
+        public override int GetHashCode() => this.Value.GetHashCode();
+        public static bool operator ==(Ipv6Cidr left, Ipv6Cidr right) => left.Equals(right);
+        public static bool operator !=(Ipv6Cidr left, Ipv6Cidr right) => !left.Equals(right);
+        public static bool operator >(Ipv6Cidr left, Ipv6Cidr right) => left.Value > right.Value;
+        public static bool operator <(Ipv6Cidr left, Ipv6Cidr right) => left.Value < right.Value;
+        public static bool operator >=(Ipv6Cidr left, Ipv6Cidr right) => left.Value >= right.Value;
+        public static bool operator <=(Ipv6Cidr left, Ipv6Cidr right) => left.Value <= right.Value;
+        
+        public static bool operator ==(Ipv6Cidr left, byte right) => left.Value == right;
+        public static bool operator !=(Ipv6Cidr left, byte right) => left.Value != right;
+        public static bool operator >(Ipv6Cidr left, byte right) => left.Value > right;
+        public static bool operator <(Ipv6Cidr left, byte right) => left.Value < right;
+        public static bool operator >=(Ipv6Cidr left, byte right) => left.Value >= right;
+        public static bool operator <=(Ipv6Cidr left, byte right) => left.Value <= right;
+        
+        public static bool operator ==(byte left, Ipv6Cidr right) => left == right.Value;
+        public static bool operator !=(byte left, Ipv6Cidr right) => left != right.Value;
+        public static bool operator >(byte left, Ipv6Cidr right) => left > right.Value;
+        public static bool operator <(byte left, Ipv6Cidr right) => left < right.Value;
+        public static bool operator >=(byte left, Ipv6Cidr right) => left >= right.Value;
+        public static bool operator <=(byte left, Ipv6Cidr right) => left <= right.Value;
+
+        public static Ipv6Cidr Parse(string? value)
+            => TryParse(value, out var result) ? result : throw new FormatException();
+
+        public static Ipv6Cidr Parse(int value)
+            => Parse((byte)value);
+        public static Ipv6Cidr Parse(byte value)
+            => TryParse(value, out var result) ? result : throw new ArgumentOutOfRangeException();
+        public static bool TryParse(byte value, out Ipv6Cidr result) 
+            => (value <= 128, value <= 128 ? new Ipv6Cidr(value) : default).Try(out result);
+
+        public static bool TryParse(string? text, out Ipv6Cidr result)
+        {
+            result = default;
+            return text is not null && Ipv6Cidr.TryParse(text, out var charsRead, out result) && charsRead == text.Length;
+        }
+
+        public static bool TryParse(string? text, out int charsRead, out Ipv6Cidr result)
+        {
+            var span = new SpanWrapper(text);
+            charsRead = default;
+            return TryParse(ref span, ref charsRead, out result);
+        }
+        
+        internal static bool TryParse(ref SpanWrapper text, ref int charsRead, out Ipv6Cidr result)
+        {
+            result = default;
+            if (!text.TryParseByte(ref charsRead, out var value) || value > 128)
+                return false;
+            result = new (value);
+            return true;
+        }
+
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER
+        public static bool TryParse(ReadOnlySpan<char> text, out int charsRead, out Ipv6Cidr result)
+        {
+            var span = new SpanWrapper(text);
+            charsRead = default;
+            return TryParse(ref span, ref charsRead, out result);
+        }
+        public static bool TryParse(ReadOnlySpan<char> text, out Ipv6Cidr result)
+            => TryParse(text, out var charsRead, out result) && charsRead == text.Length;
+#endif
+        public Ipv6SubnetMask ToSubnetMask() => this;
+    }
+}
