4ca2bbd11c740cb9520eb698fc22495e4f51e63e
Mike Christiansen <michael.a.christiansen10@gmail.com>
2021-11-25 17:00:00 -0500

    Add IPv4 JSON Converters

---
+++ src/NetworkPrimitives.Ipv4.JsonConverters/Ipv4NetworkMatchJsonConverter.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace NetworkPrimitives.Ipv4.JsonConverters
+{
+    public class Ipv4NetworkMatchJsonConverter : JsonConverter<Ipv4NetworkMatch>
+    {
+        private Ipv4NetworkMatchJsonConverter() { }
+        public static readonly Ipv4NetworkMatchJsonConverter Instance = new ();
+        public override Ipv4NetworkMatch Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options
+        ) => Ipv4NetworkMatch.Parse(reader.GetString());
+
+        public override void Write(
+            Utf8JsonWriter writer,
+            Ipv4NetworkMatch value,
+            JsonSerializerOptions options
+        ) => writer.WriteStringValue(value.ToString());
+    }
+}
